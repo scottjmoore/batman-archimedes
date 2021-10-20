f8fde32a07096232a05e77cce1fac31238246f28
Scott Moore (Arch) <scottmoore@archlinux-epsilon.broadband>
2021-10-20 23:25:47 +0100

    Add sprite shift option to create pre-shifted tiles

--- scripts/png2asm.py
+++ scripts/png2asm.py
@@ -28,6 +28,7 @@ for infile, outfile in zip(args.infile, args.outfile):
 
     sprite_width = args.spritewidth
     sprite_height = args.spriteheight
+    sprite_shift = args.spriteshift
 
     palette_name = filename + "_palette"
     palette_type,palette_data = image.palette.getdata()
@@ -36,18 +37,21 @@ for infile, outfile in zip(args.infile, args.outfile):
 
     image_pixels = image.load()
 
-    print("png2asm: '"+infile.name+"' => '"+outfile.name+"'")
-    print("\tImage size : "+f'{image_width}'+"x"+f'{image_height}')
-    print("\tSprite size : "+f'{sprite_width}'+"x"+f'{sprite_height}')
+    print("\npng2asm: '"+infile.name+"' => '"+outfile.name+"'")
+    print("\tImage size   : "+f'{image_width}'+"x"+f'{image_height}')
+    print("\tSprite size  : "+f'{sprite_width}'+"x"+f'{sprite_height}')
+    print("\tSprite shift : "+f'{sprite_shift}'+"\n")
 
     label_name = image_name.replace("-","_")
     f_out.write(label_name+':\n')
 
     iy = 0
+    tile = 0
 
     while iy < image_height:
         ix = 0
         while ix < image_width:
+            f_out.write(label_name+f'_tile_{tile}'+':\n')
             y = 0
             while y < sprite_height:
                 x = 0
@@ -68,10 +72,99 @@ for infile, outfile in zip(args.infile, args.outfile):
                     else:
                         f_out.write('\n')
                 y += 1
+
+            if sprite_shift:
+                f_out.write(label_name+f'_tile_{tile}'+'_shift_1:\n')
+                y = 0
+                while y < sprite_height:
+                    x = 1
+                    f_out.write('\t.byte\t')
+                    while x < sprite_width:
+                        b0 = image_pixels[ix + x + 0,iy + y]
+                        b1 = image_pixels[ix + x + 1,iy + y]
+                        b2 = image_pixels[ix + x + 2,iy + y]
+                        b3 = image_pixels[ix + x + 3,iy + y]
+                        b4 = image_pixels[ix + x + 4,iy + y]
+                        b5 = image_pixels[ix + x + 5,iy + y]
+                        b6 = image_pixels[ix + x + 6,iy + y]
+                        if (x + 7 < sprite_width):
+                            b7 = image_pixels[ix + x + 7,iy + y]
+                            f_out.write(f'0x{b0:02x},'+f'0x{b1:02x},'+f'0x{b2:02x},'+f'0x{b3:02x},'+f'0x{b4:02x},'+f'0x{b5:02x},'+f'0x{b6:02x},'+f'0x{b7:02x}')
+                        else:
+                            f_out.write(f'0x{b0:02x},'+f'0x{b1:02x},'+f'0x{b2:02x},'+f'0x{b3:02x},'+f'0x{b4:02x},'+f'0x{b5:02x},'+f'0x{b6:02x},')
+
+                        x += 8
+                        if x < sprite_width:
+                            f_out.write(',')
+                        else:
+                            b0 = image_pixels[ix + 0,iy + y]
+                            f_out.write(f'0x{b0:02x}'+'\n')
+                    y += 1
+
+                y = 0
+                f_out.write(label_name+f'_tile_{tile}'+'_shift_2:\n')
+                while y < sprite_height:
+                    x = 2
+                    f_out.write('\t.byte\t')
+                    while x < sprite_width:
+                        b0 = image_pixels[ix + x + 0,iy + y]
+                        b1 = image_pixels[ix + x + 1,iy + y]
+                        b2 = image_pixels[ix + x + 2,iy + y]
+                        b3 = image_pixels[ix + x + 3,iy + y]
+                        b4 = image_pixels[ix + x + 4,iy + y]
+                        b5 = image_pixels[ix + x + 5,iy + y]
+                        if (x + 7 < sprite_width):
+                            b6 = image_pixels[ix + x + 6,iy + y]
+                            b7 = image_pixels[ix + x + 7,iy + y]
+                            f_out.write(f'0x{b0:02x},'+f'0x{b1:02x},'+f'0x{b2:02x},'+f'0x{b3:02x},'+f'0x{b4:02x},'+f'0x{b5:02x},'+f'0x{b6:02x},'+f'0x{b7:02x}')
+                        else:
+                            f_out.write(f'0x{b0:02x},'+f'0x{b1:02x},'+f'0x{b2:02x},'+f'0x{b3:02x},'+f'0x{b4:02x},'+f'0x{b5:02x},')
+
+                        x += 8
+                        if x < sprite_width:
+                            f_out.write(',')
+                        else:
+                            b0 = image_pixels[ix + 0,iy + y]
+                            b1 = image_pixels[ix + 1,iy + y]
+                            f_out.write(f'0x{b0:02x},'+f'0x{b1:02x}'+'\n')
+                    y += 1
+
+                y = 0
+                f_out.write(label_name+f'_tile_{tile}'+'_shift_3:\n')
+                while y < sprite_height:
+                    x = 3
+                    f_out.write('\t.byte\t')
+                    while x < sprite_width:
+                        b0 = image_pixels[ix + x + 0,iy + y]
+                        b1 = image_pixels[ix + x + 1,iy + y]
+                        b2 = image_pixels[ix + x + 2,iy + y]
+                        b3 = image_pixels[ix + x + 3,iy + y]
+                        b4 = image_pixels[ix + x + 4,iy + y]
+                        if (x + 7 < sprite_width):
+                            b5 = image_pixels[ix + x + 5,iy + y]
+                            b6 = image_pixels[ix + x + 6,iy + y]
+                            b7 = image_pixels[ix + x + 7,iy + y]
+                            f_out.write(f'0x{b0:02x},'+f'0x{b1:02x},'+f'0x{b2:02x},'+f'0x{b3:02x},'+f'0x{b4:02x},'+f'0x{b5:02x},'+f'0x{b6:02x},'+f'0x{b7:02x}')
+                        else:
+                            f_out.write(f'0x{b0:02x},'+f'0x{b1:02x},'+f'0x{b2:02x},'+f'0x{b3:02x},'+f'0x{b4:02x},')
+
+                        x += 8
+                        if x < sprite_width:
+                            f_out.write(',')
+                        else:
+                            b0 = image_pixels[ix + 0,iy + y]
+                            b1 = image_pixels[ix + 1,iy + y]
+                            b2 = image_pixels[ix + 2,iy + y]
+                            f_out.write(f'0x{b0:02x},'+f'0x{b1:02x},'+f'0x{b2:02x}'+'\n')
+                    y += 1
+
+                tile = tile + 1
+
             ix += sprite_width
             if ix < image_width:
                 if iy <= (image_height - sprite_height):
                     f_out.write('\n')
+                        
         iy += sprite_height
 
     # if args.spriteshift:
