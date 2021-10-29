b7d02af909e5c8c785ff099c924db2e9049a9353
Scott Moore (Arch) <scottmoore@archlinux-epsilon.broadband>
2021-10-29 22:46:17 +0100

    Write LUT to build file

--- scripts/sincos.py
+++ scripts/sincos.py
@@ -2,10 +2,14 @@
 
 import math
 
-print("sin:")
-for deg in range(0,256):
-    print(f"\t.4byte {int(math.sin(math.radians(deg * (360/256))) * (1 << 16))}")
+f_out = open("./build/sincos.asm","w")
 
-print("cos:")
+f_out.write("sin:\n")
 for deg in range(0,256):
-    print(f"\t.4byte {int(math.cos(math.radians(deg * (360/256))) * (1 << 16))}")
+    f_out.write(f"\t.4byte {int(math.sin(math.radians(deg * (360/256))) * (1 << 16))}\n")
+
+f_out.write("cos:\n")
+for deg in range(0,256):
+    f_out.write(f"\t.4byte {int(math.cos(math.radians(deg * (360/256))) * (1 << 16))}\n")
+
+f_out.close
