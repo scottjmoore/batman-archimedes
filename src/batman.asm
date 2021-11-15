c0e70a027bd0e22cf8ed7467966199fffeb2ff5c
Scott Moore (Arch) <scottmoore@archlinux-epsilon.broadband>
2021-11-15 15:36:39 +0000

    Tweak y scroll values

--- src/batman.asm
+++ src/batman.asm
@@ -1229,11 +1229,11 @@ batman_cant_drop:
     MOVLT R3,#0
     CMP R1,#224
     ADDGE R3,R3,#1
-    CMP R2,#32
+    CMP R2,#80
     SUBLT R4,R4,#1
     CMP R4,#0
     MOVLT R4,#0
-    CMP R2,#128
+    CMP R2,#144
     ADDGE R4,R4,#1
 
 
